d38ba0f6e3afd8c3cf43cdb9bdaff7ef24196705
Varis <varisnithiparkorn@gmail.com>
2025-03-17 21:59:47 +0700

    edit

--- src/main/java/sit/int204/application/entities/User.java
+++ src/main/java/sit/int204/application/entities/User.java
@@ -2,12 +2,14 @@ package sit.int204.application.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name= "users")
 public class User {
     @Id
     private int id;
